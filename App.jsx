5197d3d6470e669c858ab1215b9cc068666bae20
akku27-cse <janaa7223@gmail.com>
2024-11-22 08:18:53 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react-native/no-inline-styles */
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>App</Text>
+      <Text style={{fontSize:30}}>App Developer</Text>
+      <Text style={{fontSize:30}}>Reactv Native learner</Text>
+      <Button title="press"/>
+    </View>
+  
+  )
+
+}
+
+export default App;
